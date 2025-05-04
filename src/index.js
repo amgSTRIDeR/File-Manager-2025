b77a30ff9e34d889a03ab2e6c814ffffcd17ab5a
amgstrider <amgstrider@gmail.com>
2025-05-04 19:03:04 +0400

    Feat: greeting and farewel messages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log(1)
+import logColoredMessage from "../../node-nodejs-basics/src/common/colors.js";
+
+
+const fileManager = async () => {
+    const argsArray = (process.argv).slice(2);
+    const userName = argsArray.find((arg) => arg.startsWith('--username'))?.split('=')[1] || 'guest';
+    logColoredMessage(`Welcome to the File Manager, ${userName}!`, 'magenta');
+
+    process.stdin.on('data', (chunk) => {
+        const command = chunk.toString().trim();
+
+        if(command === '.exit') {
+            process.exit();
+        }
+    })
+
+
+
+    process.on('SIGINT', () => {
+        process.exit();
+    });
+
+    process.on('exit', () => {
+        logColoredMessage(`Thank you for using File Manager, ${userName}, goodbye!`, 'magenta');
+    });
+
+};
+
+await fileManager();
